cc7cd5ba3721f0813e030cbce086c989ba57f1fc
unknown <fmjladage@gmail.com>
2018-10-31 12:14:54 +0100

    Right mouse delete block

--- Assets/CubePlacer.cs
+++ Assets/CubePlacer.cs
@@ -15,37 +15,38 @@ public class CubePlacer : MonoBehaviour
 
     private void Update()
     {
-
-
-        
-        
-
-
         if (Input.GetMouseButtonDown(0) && gameManager.money > 0)
         {
             RaycastHit[] hits;
             Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
             hits = Physics.RaycastAll(ray);
+            RaycastHit hitInfo;
 
             for (int i = 0; i < hits.Length; i++)
             {
                 RaycastHit hit = hits[i];
                 Renderer rend = hit.transform.GetComponent<Renderer>();
-
-                Debug.Log(hit.collider.name);
             }
-
-
-            RaycastHit hitInfo;
-
 
             if (Physics.Raycast(ray, out hitInfo) && hits.Length < 2)
             {
-                
-
-
                 gameManager.BuyTile();
                 PlaceCubeNear(hitInfo.point);
+            }
+        }
+
+        if (Input.GetMouseButtonDown(1))
+        {
+            RaycastHit[] hits;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            hits = Physics.RaycastAll(ray);
+
+            foreach(var x in hits)
+            {
+                if(x.collider.tag == "Block")
+                {
+                    Destroy(x.collider.gameObject);
+                }
             }
         }
     }
@@ -54,11 +55,8 @@ public class CubePlacer : MonoBehaviour
     {
         var finalPosition = grid.GetNearestPointOnGrid(clickPoint);
         Instantiate(block, finalPosition, Quaternion.identity);
-
-        //GameObject.CreatePrimitive(PrimitiveType.Sphere).transform.position = nearPoint;
     }
 
-    //
     private void TempCube(Vector3 clickPoint)
     {
         var finalPosition = grid.GetNearestPointOnGrid(clickPoint);
