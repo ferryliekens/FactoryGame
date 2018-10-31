ca270a33659ccebe42b5ce332a5172e1a50cd5bb
unknown <fmjladage@gmail.com>
2018-10-31 12:02:04 +0100

    No double placement on same tile

--- Assets/CubePlacer.cs
+++ Assets/CubePlacer.cs
@@ -6,6 +6,8 @@ public class CubePlacer : MonoBehaviour
     public GameManager gameManager;
     public Transform block;
 
+
+
     private void Awake()
     {
         grid = FindObjectOfType<Grid>();
@@ -13,20 +15,39 @@ public class CubePlacer : MonoBehaviour
 
     private void Update()
     {
+
+
+        
+        
+
+
         if (Input.GetMouseButtonDown(0) && gameManager.money > 0)
         {
-            RaycastHit hitInfo;
+            RaycastHit[] hits;
             Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            hits = Physics.RaycastAll(ray);
 
-            gameManager.BuyTile();
-
-            if (Physics.Raycast(ray, out hitInfo))
+            for (int i = 0; i < hits.Length; i++)
             {
+                RaycastHit hit = hits[i];
+                Renderer rend = hit.transform.GetComponent<Renderer>();
+
+                Debug.Log(hit.collider.name);
+            }
+
+
+            RaycastHit hitInfo;
+
+
+            if (Physics.Raycast(ray, out hitInfo) && hits.Length < 2)
+            {
+                
+
+
+                gameManager.BuyTile();
                 PlaceCubeNear(hitInfo.point);
             }
         }
-
-
     }
 
     private void PlaceCubeNear(Vector3 clickPoint)
